76d2d11b17c944cd707ad1092eba9c9742cba919
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-01-07 10:58:37 +0100

    Fix wheel location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,6 @@ set PENDULUM_EXTENSIONS=1
 
 maturin build -vv -j %CPU_COUNT% --release --strip --manylinux off --interpreter=%PYTHON%
 
-FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set pendulum_wheel=%%i
+FOR /F "delims=" %%i IN ('dir /s /b rust\target\wheels\*.whl') DO set pendulum_wheel=%%i
 
 %PYTHON% -m pip install --no-deps %pendulum_wheel% -vv

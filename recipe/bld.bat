69b656433ca595685dcf5201382211ea0c1f9bca
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2024-07-04 16:59:26 +0200

    Cleanup lints

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,4 +4,4 @@ maturin build -vv -j %CPU_COUNT% --release --strip --manylinux off --interpreter
 
 FOR /F "delims=" %%i IN ('dir /s /b rust\target\wheels\*.whl') DO set pendulum_wheel=%%i
 
-%PYTHON% -m pip install --no-deps %pendulum_wheel% -vv
+%PYTHON% -m pip install --no-deps %pendulum_wheel% -vv --no-build-isolation
